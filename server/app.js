a193d4b76d6e797cdd5a4f383e330bee85fa5d1b
Alfitonauval <nauvalalfito44@gmail.com>
2025-04-19 22:23:28 +0700

    Adding post tips and trick

--- server/app.js
+++ server/app.js
@@ -1,23 +1,32 @@
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
-import RegisterAdminRoutes from "./src/routes/admin/RegisterAdminRoutes.js"
-import LoginAdminRoutes from "./src/routes/admin/LoginAdminRoutes.js"
+import multer from "multer";
+import upload from "./src/middleware/Storage.js";
+
+import RegisterAdminRoutes from "./src/routes/admin/RegisterAdminRoutes.js";
+import LoginAdminRoutes from "./src/routes/admin/LoginAdminRoutes.js";
+import PostTips from "./src/controllers/Tips/PostTips.js"
 const app = express();
 const port = process.env.PORT;
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+const tipsUpload = upload.fields([
+    { name: "Thumbnail", maxCount: 1 },
+    { name: "Image", maxCount: 1 },
+  ]);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("working");
 });
 
-app.post("/api/v2/admin/register",RegisterAdminRoutes)
-app.post("/api/v2/admin/login",LoginAdminRoutes)
-
+// Admin
+app.post("/api/v2/admin/register", RegisterAdminRoutes);
+app.post("/api/v2/admin/login", LoginAdminRoutes);
+app.post("/api/v2/post/tips",tipsUpload,PostTips)
 
 app.listen(port, () => {
   console.log(`http://localhost:5000`);
 });
-
